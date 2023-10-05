c49fa8e8ee40be5238ebd89d177b70192f79c46e
Noah Jackson <nojackson99@gmail.com>
2023-10-05 17:45:07 -0400

    can start and stop timer with button, timer ui needed

--- Source/PomTimer.Client/NpmJS/src/index.js
+++ Source/PomTimer.Client/NpmJS/src/index.js
@@ -1,14 +1,47 @@
 import Timer from 'easytimer.js';
 
-window.testTimer = (message) => {
-	console.log(message);
+//* This is a example function testing JS interop with easytimer.js
+// window.testTimer = (message) => {
+// 	console.log(message);
 
-	const timer = new Timer();
-	timer.start();
+// 	const timer = new Timer();
+// 	timer.start();
+
+// 	timer.addEventListener('secondsUpdated', function (e) {
+// 		console.log(timer.getTimeValues().toString());
+// 	});
+
+// 	return;
+// };
+
+var timer = null;
+
+window.startTimer = () => {
+	console.log('Starting timer...');
+
+	timer = new Timer();
+	timer.start({ countdown: true, startValues: { minutes: 25 } });
 
 	timer.addEventListener('secondsUpdated', function (e) {
 		console.log(timer.getTimeValues().toString());
 	});
+};
 
-	return;
+window.handleTimerAction = () => {
+	if (!timer) {
+		window.startTimer();
+
+		return 'Pause';
+	}
+	if (timer.isRunning()) {
+		console.log('Pausing timer...');
+		timer.pause();
+
+		return 'Resume';
+	} else {
+		console.log('Resuming timer...');
+		timer.start();
+
+		return 'Pause';
+	}
 };
